1f1aa3ea53a9766c09e0bb63aea82441709cf99b
Ramya Challa <ramyac6@cs.washington.edu>
2023-01-24 13:06:26 -0800

    completed 3.1

--- picarx/sensor.py
+++ picarx/sensor.py
@@ -1,12 +1,21 @@
 from picarx_improved import Picarx
 
 
-class Maneuvering(object):
-    def __init__(self):
-        self.pi = 4
+class Sensors(object):
+    def __init__(self,pin0,pin1,pin2):
+        self.chn0 = Picarx.ADC(pin0)
+        self.chn1 = Picarx.ADC(pin1)
+        self.chn2 = Picarx.ADC(pin2)
 
+    def read(self):
+        adc_value_list = []
+        adc_value_list.append(self.chn0.read())
+        adc_value_list.append(self.chn1.read())
+        adc_value_list.append(self.chn2.read())
+        return adc_value_list
 
 if __name__ == "__main__":
-    print()
+    sensor = Sensors(0,1,2)
+    print(sensor.read())
 
     
