6951b35283d9df001a2f7437738b56fc32edf762
Ramya Challa <ramyac6@cs.washington.edu>
2023-01-24 13:11:20 -0800

    created controller class

---
+++ picarx/interpreter.py
@@ -0,0 +1,14 @@
+from picarx_improved import Picarx
+
+
+class Interpreter(object):
+    def __init__(self,sensitivity=50,polarity=50):
+        self.sensitivity = sensitivity
+        self.polarity = polarity
+
+    def processing(self, adc_value_list):
+        
+        return adc_value_list
+
+if __name__ == "__main__":
+    print()
