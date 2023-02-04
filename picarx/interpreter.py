45a9eea1bf8c1ef9292210c72644e944365e6f82
Ramya Challa <ramyac6@cs.washington.edu>
2023-02-04 00:14:42 -0800

    added comments for interpreter

--- picarx/interpreter.py
+++ picarx/interpreter.py
@@ -5,7 +5,7 @@ class Interpreter(object):
     def __init__(self,sensitivity=0.5,polarity=1):
         self.sensitivity = sensitivity * polarity
 
-    def processing(self, values):
+    def processing1(self, values):
         left, middle, right = values
 
         if right - left > 0: # means we're left of the line
@@ -14,6 +14,25 @@ class Interpreter(object):
             turn = -1 * (middle - right) / (middle + right)
 
         return turn * sensitivity
+
+    def processing(self, values):
+        # thresholds for marking
+        similarity =
+        difference = 
+
+        # if left and middle are same and left and right are different
+            # hard right
+        # if righ tand middle are same and left and right are different 
+            # slight right
+        # if left and middle are same and left and right are different
+            # light left
+        # if right and middle are same and left and right are different 
+            # hard left
+        # if right and left are same and right is light
+            # we're good
+        # if right and left are same and right is light
+            # uhhhhhh
+
 
 if __name__ == "__main__":
     print()
