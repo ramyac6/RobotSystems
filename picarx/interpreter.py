c8213060b084f84c3c4f589979b3720d17e1956d
Ramya Challa <challara@oregonstate.edu>
2023-02-04 00:04:13 -0800

    it kind of works?

--- picarx/interpreter.py
+++ picarx/interpreter.py
@@ -6,14 +6,15 @@ class Interpreter(object):
         self.sensitivity = sensitivity * polarity
 
     def processing(self, values):
+        values = [x+1 if x==0 else x for x in values]
         left, middle, right = values
 
         if right - left > 0: # means we're left of the line
             turn = -1 * (middle - right) / (middle + right)
         else:
-            turn = -1 * (middle - right) / (middle + right)
+            turn = (middle - left) / (middle + left)
 
-        return turn * sensitivity
+        return turn * self.sensitivity
 
 if __name__ == "__main__":
     print()
